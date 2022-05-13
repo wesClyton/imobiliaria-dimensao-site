f0477258b902d63a142a7957ecb9d2465e714a06
wesClyton <wes.hinsch@gmail.com>
2022-05-13 00:30:41 -0300

    API google maps quase lá

---
+++ src/app/modules/discover/discover.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { DiscoverComponent } from './discover.component';
+import { GoogleMapsModule } from '@angular/google-maps';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DiscoverComponent
+  }
+];
+
+@NgModule({
+  declarations: [ DiscoverComponent ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    GoogleMapsModule
+  ]
+})
+export class DiscoverModule {}
