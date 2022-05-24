39aa1188a603688ef3e6516136313270030b396d
Felipe Mantovani <felipexmantovani@gmail.com>
2022-05-24 19:51:31 -0300

    Ajusta máscaras

--- src/app/shared/directives/currency-br/currency-br.directive.ts
+++ src/app/shared/directives/currency-br/currency-br.directive.ts
@@ -1,8 +1,7 @@
-import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { CurrencyBrPipe } from '../../pipes/currency-br/currency-br.pipe';
-import { StringUtil } from '../../utils/string.util';
 
 @Directive({
   selector: '[appCurrencyBr]',
@@ -19,7 +18,7 @@ export class CurrencyBrDirective implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.setMask((this.elementRef.nativeElement as HTMLInputElement).value);
+    this.setMask((this.elementRef.nativeElement as HTMLInputElement).value, true);
     this.subscription.add(this.ngControl.control?.valueChanges.subscribe(value => this.setMask(value)));
   }
 
@@ -27,11 +26,60 @@ export class CurrencyBrDirective implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  private setMask(value: string): void {
+  @HostListener('blur', ['$event'])
+  private setDigits(event: any): void {
+    const value: string = event.target.value;
+    this.setMask(this.formatDigits(value));
+  }
+
+  private setMask(value: string, loadedValue: boolean = false): void {
+    let thousand!: string;
+    let decimal!: string;
+    let formated!: string;
+
+    if (loadedValue) {
+      if (!value.includes('.')) {
+        value = `${value}.00`;
+      }
+      if (value.includes('.')) {
+        decimal = value.substring(value.length - 2, value.length);
+        if (decimal.includes('.')) {
+          value = `${value}0`;
+        }
+      }
+
+      thousand = value.substring(0, value.length - 2);
+      decimal = value.substring(value.length - 2, value.length);
+      formated = thousand ? `${thousand},${decimal}` : '0';
+    }
+
     this.ngControl.control?.setValue(
-      this.currencyBrPipe.transform(StringUtil.removeSymbolCurrencyBr(value)?.toString()),
+      this.currencyBrPipe.transform(formated ? formated : value, true),
       { emitEvent: false }
     );
+  }
+
+  private formatDigits(value: string): string {
+    const thousand = value.split(',')[0];
+    let digits = value.split(',')[1];
+
+    if (digits && digits.length === 2) {
+      return value;
+    }
+
+    if (!thousand) {
+      return '0,00';
+    }
+
+    if (!digits && digits !== '' && thousand) {
+      value = `${value},00`;
+    } else if (digits === '') {
+      value = `${value}00`;
+    } else if (digits && digits.length === 1) {
+      value = `${value}0`;
+    }
+
+    return value;
   }
 
 }
