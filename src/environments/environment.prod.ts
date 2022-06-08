441b261462ad4f70b7c74957a97e127020b6ad43
Felipe Mantovani <felipexmantovani@gmail.com>
2022-06-07 23:52:54 -0300

    Remove /site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api: 'https://api.imobiliariadimensao.com.br/site',
   aws: 'https://imobiliaria-dimensao.s3.amazonaws.com',
-  baseUrl: '/site/'
+  baseUrl: '/'
 };
