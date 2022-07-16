eeee017a1687e8041a72e2f08d09d06ef7847e5e
Felipe Mantovani <felipexmantovani@gmail.com>
2022-07-16 12:27:57 -0300

    Cria MetaTagService

---
+++ src/app/shared/interfaces/page-meta-tag.interface.ts
@@ -0,0 +1,8 @@
+export interface PageMetaTag {
+  title: string;
+  description: string;
+  image?: string;
+  author?: string;
+  keywords?: Array<string>;
+  type?: string;
+}
---
+++ src/app/shared/services/meta-tag/meta-tag.service.ts
@@ -0,0 +1,58 @@
+import { Inject, Injectable } from '@angular/core';
+import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { APP_CONFIG } from '../../../app.config';
+import { HOST_URL } from '../../../tokens/host-url';
+import { PageMetaTag } from '../../interfaces/page-meta-tag.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MetaTagService {
+
+  private readonly default: PageMetaTag = {
+    author: APP_CONFIG.name,
+    image: '',
+    description: '',
+    keywords: [''],
+    title: APP_CONFIG.name,
+    type: 'website'
+  }
+
+  constructor(
+    private readonly metaService: Meta,
+    private readonly titleService: Title,
+    @Inject(HOST_URL)
+    private readonly hostUrl: string,
+    private readonly router: Router
+  ) { }
+
+  public add(metaTag: PageMetaTag = this.default, index: boolean = false): void {
+    const pageMetadata: PageMetaTag = {...this.default, ...metaTag};
+    const metatags: MetaDefinition[] = this.generateMetaDefinitions(pageMetadata);
+
+    this.metaService.addTags([
+     ...metatags,
+     { property: 'og:url', content: `${this.hostUrl}${this.router.url}`},
+     { name: 'robots', content: index ? 'index, follow' : 'noindex' },
+     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+     { 'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8' },
+    ]);
+
+    this.titleService.setTitle(pageMetadata.title);
+  }
+
+  private generateMetaDefinitions(metaTag: Partial<PageMetaTag>): Array<MetaDefinition> {
+    return [
+      { name: 'title', content: metaTag.title || this.default.title },
+      { property: 'og:title', content: metaTag.title || this.default.title },
+      { name: 'description', content: metaTag.description || this.default.description },
+      { property: 'og:description', content: metaTag.description || this.default.description },
+      { name: 'author', content: metaTag.author || this.default.author || '' },
+      { property: 'og:author', content: metaTag.author || this.default.author || '' },
+      { name: 'keywords', content: metaTag?.keywords?.join(', ') || this.default.keywords?.join(', ') || '' },
+      { property: 'og:type', content: metaTag.type || this.default.type || '' }
+    ];
+  }
+
+}
